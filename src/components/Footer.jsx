a2fc73e1be81b99eb29ff7bd4c52c3e6618f5e31
Dhruvin <therockstar3007@gmail.com>
2024-10-23 01:33:18 +0530

    Improved UI

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,9 @@ import { Link } from "react-router-dom"
 export const Footer = () => {
   return (
     <footer>
-      <p>© 2030 <Link to="/">MindFlect</Link>. All Rights Reserved.</p>
+      <p>© 2030 <Link to="/">MindFlect</Link></p>
+      <span>Made with <heart>❤</heart>  by Dhruvin</span> 
+      <p>All Rights Reserved</p> 
     </footer>
   )
 }
