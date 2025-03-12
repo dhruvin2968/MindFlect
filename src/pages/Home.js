cb12d4efaa51b6a4bdb87efe7b60b8864b5e059c
Dhruvin <therockstar3007@gmail.com>
2025-03-12 10:59:47 +0530

    GSAP commit

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,18 @@ import { useTitle } from "../hooks/useTitle";
 import { PostCard, SkeletonCard } from "../components";
 import { getDocs, collection } from "firebase/firestore";
 import { db } from "../firebase/config";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+gsap.registerPlugin(ScrollTrigger);
+
 export const Home = () => {
   useTitle("MindFlect - Reflect, Share, Grow");
   const [posts, setPosts] = useState(new Array(2).fill(false)); //returns array with 2 elements both are false
   const [toggle, setToggle] = useState();
   const postRef = useRef(collection(db, "posts"));
+  const cardsRef = useRef([]);
+
   //everytime we r on homepage it should load all posts so we use useeffect
   useEffect(() => {
     async function getPosts() {
@@ -24,20 +31,49 @@ export const Home = () => {
     getPosts();
   }, [toggle]); // everytime toggle changes we reload our documents
 
-  // usko postRef as dependecy chaiye but it will create infinite calls
+  // usko postRef as dependency chaiye but it will create infinite calls
   //as objects and fns dependency mei nhi daalte
   //so we use useRef
+
+  // Scroll-based animation
+  useEffect(() => {
+    if (cardsRef.current.length > 0) {
+      cardsRef.current.forEach((card, index) => {
+        if (card) {
+          gsap.fromTo(
+            card,
+            { opacity: 0, y: 50 },
+            {
+              opacity: 1,
+              y: 0,
+              duration: 0.8,
+              ease: "power2.out",
+              scrollTrigger: {
+                trigger: card,
+                start: "top 80%", // when card enters viewport
+                toggleActions: "play none none none",
+              },
+            }
+          );
+        }
+      });
+    }
+  }, [posts]);
 
   return (
     <section>
       {posts.map((post, index) =>
         post ? (
-          <PostCard
+          <div
             key={post.id}
-            post={post}
-            toggle={toggle}
-            setToggle={setToggle}
-          />
+            ref={(el) => (cardsRef.current[index] = el)}
+          >
+            <PostCard
+              post={post}
+              toggle={toggle}
+              setToggle={setToggle}
+            />
+          </div>
         ) : (
           <SkeletonCard key={index} />
         )
